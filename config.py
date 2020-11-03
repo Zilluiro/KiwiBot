5eb1ac0717f04e376f4ff2eda9cae31d9b7d3d99
Alexander Baransky <alexander.baranskiy@yandex.ru>
2020-11-03 19:42:59 +0300

    Update 18+ tags
    For better filtering

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import logging
 
 
-tags = ["no_bra", "tits", "boobs", "anus", "pussy", "uncensored", "censored", "open_shirt", "trap", "yaoi", "cum",
-        "penis", "milk", "sex"]
+tags = ["no_bra", "tits", "boobs", "anus", "pussy", "pussy_juice", "uncensored", "censored", "open_shirt", "trap", "yaoi", "cum",
+        "penis", "milk", "sex", "vibrator", "naked", "breasts", "breast_grab", "nipples", "ass", "naked_cape", "pubic_hair"]
 
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
